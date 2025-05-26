ccecd783479509a37f1f7100c2e872765dc6c999
igorw33 <igorwalasek@onet.pl>
2025-05-26 16:10:49 +0200

    Simple server that serves a page with three.js

---
+++ static/scripts/script.js
@@ -0,0 +1,72 @@
+import * as THREE from 'three';
+
+window.addEventListener("load", function () {
+    // scena 3D
+
+    const scene = new THREE.Scene();
+
+    // kamera perspektywiczna - przeczytaj dokładnie objaśnienia w komentarzach
+
+    const camera = new THREE.PerspectiveCamera(
+        45,    // kąt patrzenia kamery (FOV - field of view)
+        4 / 3,    // proporcje widoku, powinny odpowiadać proporcjom ekranu przeglądarki użytkownika
+        0.1,    // minimalna renderowana odległość
+        10000    // maksymalna renderowana odległość od kamery
+    );
+
+    // renderer wykorzystujący WebGL - działa stabilnie na wszystkich
+    // najnowszych przeglądarkach zarówno desktopowych jak mobilnych
+
+    const renderer = new THREE.WebGLRenderer();
+
+    // kolor tła sceny - uwaga na prefix 0x a nie #
+
+    renderer.setClearColor(0x0066ff);
+
+    // ustal rozmiary renderowanego okna w px (szer, wys)
+
+    renderer.setSize(500, 500);
+
+    // dodanie renderera do diva, który istnieje na scenie
+
+    this.document.getElementById("root").append(renderer.domElement);
+
+    camera.position.x = -200;
+    camera.position.y = 200;
+    camera.position.z = 200;
+
+    // lub
+
+    camera.position.set(100, 100, 100)
+
+    // nakierowanie kamery na punkt (0,0,0) w przestrzeni (zakładamy, że istnieje już scena)
+
+    camera.lookAt(scene.position);
+
+    // kluczowy element - animacja
+
+    function render() {
+
+
+        //w tym miejscu ustalamy wszelkie zmiany w projekcie (obrót, skalę, położenie obiektów)
+        //np zmieniająca się wartość rotacji obiektu
+
+        //mesh.rotation.y += 0.01;
+
+        //wykonywanie funkcji bez końca, ok 60 fps jeśli pozwala na to wydajność maszyny
+
+        requestAnimationFrame(render);
+
+        // potwierdzenie w konsoli, że render się wykonuje
+
+        console.log("render leci")
+
+        //ciągłe renderowanie / wyświetlanie widoku sceny naszą kamerą
+
+        renderer.render(scene, camera);
+    }
+
+    // na koniec jednokrotne wykonanie powyższej funkcji
+
+    render();
+})
